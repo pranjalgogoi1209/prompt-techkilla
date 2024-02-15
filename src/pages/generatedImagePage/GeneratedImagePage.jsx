63c9d2cd3735f0e27037b2d91726c47a3dbde395
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-02-15 19:50:06 +0530

    api update

--- src/pages/generatedImagePage/GeneratedImagePage.jsx
+++ src/pages/generatedImagePage/GeneratedImagePage.jsx
@@ -45,7 +45,7 @@ export default function GeneratedImagePage({ capturedImage }) {
       showImgRef.current.style.display = "flex";
       setGeneratedImg("");
       axios
-        .post("https://eecc-103-17-110-127.ngrok-free.app/img2txt", {
+        .post("https://49bb-103-17-110-127.ngrok-free.app/img2txt", {
           data: prompt,
           image: capturedImage.split(",")[1],
           // image: capturedImage,
